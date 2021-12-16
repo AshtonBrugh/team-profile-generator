1e9b408f05c8d06ac4b3eadfb524db66803ecd7c
Ashton B <ashtonbrugh@gmail.com>
2021-12-16 10:59:07 -0500

    adding tests to the Employee object

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,39 @@
 const Employee = require('../lib/Employee.js');
 
-test("check to see if Employee is instantiated", ()=> {
-    const newEmployee = new Employee('name');
+test('creates an employee object', ()=> {
+    const newEmployee = new Employee('Ashton', 12, 'ashtonbrugh@gmail.com');
 
-    expect(typeof (newEmployee)).toBe("object");
+    expect(newEmployee.name).toEqual(expect.any(String));
+    expect(newEmployee.id).toEqual(expect.any(Number));
+    expect(newEmployee.email).toEqual(expect.any(String));
     
 });
 
-test("")
+test('gets employee name', () => {
+    const newEmployee = new Employee('Ashton', 12, 'ashtonbrugh@gmail.com');
+
+    expect(newEmployee.getName()).toEqual(expect.any(String));
+});
+
+test('get employee id', () => {
+    const newEmployee = new Employee('Ashton', 12, 'ashtonbrugh@gmail.com');
+
+    expect(newEmployee.getId()).toEqual(expect.any(Number));
+});
+
+test('get employee email', () => {
+    const newEmployee = new Employee('Ashton', 12, 'ashtonbrugh@gmail.com');
+
+    expect(newEmployee.getEmail()).toEqual(expect.any(String));
+});
+
+test('get employee role', () => {
+    const newEmployee = new Employee('Ashton', 12, 'ashtonbrugh@gmail.com');
+
+    expect(newEmployee.getRole()).toEqual('Employee');
+})
+
+
 
 
 
